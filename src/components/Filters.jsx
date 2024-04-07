0eba100caac65b8c2a6af79b1655a334800437fb
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-04-07 18:46:58 +0100

    Rating component completed and added to the Filter component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Filters = () => {
+    const [rate, setRate] = useState(2);
   return (
     <div className="filters">
       <span className="title">Filter Products</span>
@@ -35,10 +37,10 @@ const Filters = () => {
           id={`inline-4`}
         />
       </span>
-      {/* <span>
+      <span>
         <label style={{paddingRight: 10}}>Rating: </label>
-        <Rating rating={byRating} style={{ cursor: "pointer"}}/>
-      </span> */}
+        <Rating rating={rate} onClick={(i) => setRate(i + 1)} style={{ cursor: "pointer"}}/>
+      </span>
       <Button variant="light">Clear Filters</Button>
     </div>
   );
